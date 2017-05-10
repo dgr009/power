3e63de854fc62f392500ef4f45511e55dced134b
lee <lee@192.168.0.5>
2017-05-10 23:53:53 +0900

    ttt

--- palette/src/main/java/com/icia/palette/controller/ProductController.java
+++ palette/src/main/java/com/icia/palette/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.icia.palette.controller;
 
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.icia.palette.service.*;
 
@@ -9,5 +11,9 @@ import com.icia.palette.service.*;
 public class ProductController {
 	@Autowired
 	private ProductService service;
-	
+	@RequestMapping(value="/productRegister",method=RequestMethod.GET)
+	public String productRegister(){
+		return "products/ProductRegister";
+		
+	}
 }
