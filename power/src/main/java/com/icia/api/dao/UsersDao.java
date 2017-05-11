c8c2b4d978398a145ebb9550726664f4bf543687
Administrator <Administrator@Admin-PC>
2017-05-11 18:24:57 +0900

    sysysy

--- power/src/main/java/com/icia/api/dao/UsersDao.java
+++ power/src/main/java/com/icia/api/dao/UsersDao.java
@@ -65,29 +65,62 @@ public class UsersDao {
 		map.put("start", start);
 		map.put("end", end);
 		map.put("orderId", orderId);
-		List<BookmarkList> list = tpl.selectList("api.dao.UsersDao.usersBookmarkList",map);
-		return list;
+		return tpl.selectList("api.dao.UsersDao.usersBookmarkList",map);
 	}
-	
-	
-	public void main(){
-		
+	//주문내역보기
+	public List<OrderList> orderList(String orderId,int start,int end){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("start", start);
+		map.put("end", end);
+		map.put("orderId", orderId);
+		return tpl.selectList("api.dao.UsersDao.usersOrderList",map);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	//주문 취소하기
+	public int orderDelete(int orderNo){
+		return tpl.delete("api.dao.UsersDao.usersOrderDelete",orderNo);
+	}
+	//포인트 충전하기
+	public int chargePoint(String userId,int tradePoint){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("userId", userId);
+		map.put("tradePoint", tradePoint);
+		return tpl.update("api.dao.UsersDao.usersChargePoint",map);
+	}
+	//포인트 환급하기
+	public int refundPoint(String userId,int tradePoint){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("userId", userId);
+		map.put("tradePoint", tradePoint);
+		return tpl.update("api.dao.UsersDao.usersRefundPoint",map);
+	}
+	//포인트 내역확인
+	public List<PointStatement> tradeList(String userId){
+		return tpl.selectList("api.dao.UsersDao.usersTradeList",userId);
+	}
+	//개인 회원 정보 보기
+	public Users userInfo(String userId){
+		return tpl.selectOne("api.dao.UsersDao.usersInfo",userId);
+	}
+	//개인 회원 정보 변경
+	public int userUpdate(Users user){
+		return tpl.update("api.dao.UsersDao.usersUpdate",user);
+	}
+	//개인 회원 비활성화
+	public int userDelete(String userId){
+		return tpl.update("api.dao.UsersDao.usersDelete",userId);
+	}
+	//장바구니 리스트
+	public List<Basket> basketList(String userId,int start,int end){
+		Map<String,Object> map = new HashMap<String,Object>();
+		map.put("start", start);
+		map.put("end", end);
+		map.put("userId", userId);
+		return tpl.selectList("api.dao.UsersDao.usersBasketList",map);
+	}
+	//장바구니 상품 삭제하기
+	public int basketDelete(String userId){
+		return tpl.delete("api.dao.UsersDao.usersBasketDelete",userId);
+	}
+
 	
 }
