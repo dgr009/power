6ff08c8d2511a60fd89df3898c4b7c18806defa9
lee <lee@192.168.0.7>
2017-06-05 00:07:48 +0900

    주문화면으로갈떄 수정해버려

--- palette/src/main/java/com/icia/palette/filter/UserLoginFilter.java
+++ palette/src/main/java/com/icia/palette/filter/UserLoginFilter.java
@@ -16,17 +16,17 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.userdetails.User;
 
+import com.icia.palette.vo.Admin;
 import com.icia.palette.vo.Users;
 
 
-@WebFilter("/miniHome/productOrder/*")
-public class OrderLoginFilter implements Filter {
+@WebFilter("/users/*")
+public class UserLoginFilter implements Filter {
 	private ArrayList<String> whiteList = new ArrayList();
-    public OrderLoginFilter() {
+    public UserLoginFilter() {
     }
 	public void destroy() {
 	}
-
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
@@ -35,30 +35,20 @@ public class OrderLoginFilter implements Filter {
 		HttpServletResponse res = (HttpServletResponse)response;
 		HttpSession session = req.getSession();
 		String uri = req.getRequestURI();
-
+		int a=uri.indexOf("/", 3);
+		String destination=uri.substring(a+1);
 		String go = uri;
-		int c=uri.indexOf("/", 3);
-		System.out.println("여기모냐"+c);
-		String destination=uri.substring(c+1);
 		Users user=(Users)session.getAttribute("user");
 		// 화이트리스트에 없는 로그인이 필요한 경로에 접근했고 로그인이 안된 경우
-		if((!whiteList.contains(uri)) && user==null) {
+		if(((!whiteList.contains(uri)) &&user==null)) {
 			System.out.println("로그인 안됨");
-			int itemPrice=Integer.parseInt(req.getParameter("itemPrice"));
-			int itemNo=Integer.parseInt(req.getParameter("itemNo"));
-			String itemName=req.getParameter("itemName");
-			int itemSize=Integer.parseInt(req.getParameter("itemSize"));
-			String itemOption=req.getParameter("itemOption");
-			int itemInven=Integer.parseInt(req.getParameter("itemInven"));
 			// 예를 들어 /board/view를 요청했는데 로그인이 안된 경우
 			// 로그인을 한 다음 /board/view로 다시 이동해야 한다
 			// 따라서 로그인 후 다시 이동할 주소를 세션에 저장한 다음 로그인으로 이동
-			System.out.println("야찍어봐이거"+itemName);
-			String destination2=destination+"?itemNo="+itemNo+"&itemPrice="+itemPrice+"&itemName="+itemName+"&itemSize="+itemSize+"&itemOption="+itemOption+"&itemInven="+itemInven;
 			go = "/palette/login";
-			session.setAttribute("destination", destination2);
-			if(req.getParameter("bunho")!=null)
-				session.setAttribute("bunho", req.getParameter("bunho"));
+			session.setAttribute("destination",destination);
+			//if(req.getParameter("bunho")!=null)
+				//session.setAttribute("bunho", req.getParameter("bunho"));
 			res.sendRedirect(go);
 		} else {
 		chain.doFilter(request, response);
