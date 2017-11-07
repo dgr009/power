1b7b0066f46e5c9b8217f007e72c6ad8c161fb38
sy <cruiser594@gmail.com>
2017-11-07 11:04:12 +0900

    dd

--- palette/src/main/java/com/icia/palette/StaticResourceConfiguration.java
+++ palette/src/main/java/com/icia/palette/StaticResourceConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.PathResourceResolver;
+
 @Configuration
 public class StaticResourceConfiguration extends WebMvcConfigurerAdapter{
 	 private static final String[] CLASSPATH_RESOURCE_LOCATIONS = { 
